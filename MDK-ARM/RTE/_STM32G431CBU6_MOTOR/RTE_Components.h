d55caff3eb914951341e934db826be40124f2b33
122Ben <zhenyang2017@gmail.com>
2026-08-12 15:54:52 +0800

    Add STM32CubeMX generated STM32G431CBU6 motor project

---
+++ MDK-ARM/RTE/_STM32G431CBU6_MOTOR/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32G431CBU6_MOTOR' 
+ * Target:  'STM32G431CBU6_MOTOR' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
